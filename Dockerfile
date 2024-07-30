1c0d43f4e195417e08a2e05a13f205c01e3b7a76
yashiyoshi <93021275+yashiyoshi@users.noreply.github.com>
2024-07-31 03:01:19 +0800

    feat: docker CI/CD, working app.py
    CI/CD via github actions

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
-# Use the official PyTorch image as a base image
-FROM pytorch/pytorch:latest
+FROM python:3.12.2-slim
 
-# Set the working directory
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
-COPY . /app
-
-# Install any needed packages specified in requirements.txt
+# Copy requirements file and install dependencies
+COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Run app.py when the container launches
+COPY . .
+
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,35 +1,15 @@
-# app.py
 import torch
 import torchvision.models as models
-from torchvision import transforms
-from PIL import Image
-import sys
 
-# Load pre-trained ResNeXt model
+# resnext with 50 layers and C=32
 model = models.resnext50_32x4d(pretrained=True)
 model.eval()
 
-# Define image preprocessing steps
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+# dummy data
+dummy_image = torch.rand(1, 3, 224, 224) # 1 image/batch, 3 channels, 224x224
 
-def predict(img_path):
-    img = Image.open(img_path)
-    img_t = preprocess(img)
-    batch_t = torch.unsqueeze(img_t, 0)
+with torch.no_grad():
+    output = model(dummy_image)
 
-    with torch.no_grad():
-        out = model(batch_t)
-        _, predicted = torch.max(out, 1)
-        return predicted.item()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python app.py <image_path>")
-    else:
-        prediction = predict(sys.argv[1])
-        print(f'Predicted class index: {prediction}')
+# Print the output
+print(output)
